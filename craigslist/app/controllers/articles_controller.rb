cc3a7dc25b344667b08bcfae9fe399c93ec395d4
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-06 16:26:06 -0400

    Add new article, fix destroy article

--- craigslist/app/controllers/articles_controller.rb
+++ craigslist/app/controllers/articles_controller.rb
@@ -1,5 +1,20 @@
 class ArticlesController < ApplicationController
 
+  def new
+    @category = Category.find(params[:category_id])
+    @article = @category.articles.new
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @article = @category.articles.new(article_params)
+    if @article.save
+      redirect_to category_article_path(@category, @article)
+    else
+      render 'New'
+    end
+  end
+
   def show
     @category = Category.find(params[:category_id])
     @article = @category.articles.find(params[:id])
@@ -20,11 +35,11 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @category = Category.find(params[:category_id])
     @article = @category.articles.find(params[:id])
     @article.destroy
-    redirect_to :category => 'Show'
+    redirect_to @category
   end
 
   private
