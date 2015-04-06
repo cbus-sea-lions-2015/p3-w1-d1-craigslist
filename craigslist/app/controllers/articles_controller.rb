2c54bf7d48925fa731f1d1066a01fb60393c0022
Elizabeth Blackburn <e.r.blackburn@gmail.com>
2015-04-06 18:01:50 -0400

    locked down routes/views

--- craigslist/app/controllers/articles_controller.rb
+++ craigslist/app/controllers/articles_controller.rb
@@ -1,15 +1,15 @@
 class ArticlesController < ApplicationController
+  before_filter :set_category
+  before_filter :set_user_article, only: [:edit, :update, :destroy]
+  before_filter :set_article, only: [:new, :create, :show]
 
-
+  before_filter :authenticate, except: :show
 
   def new
-    @category = Category.find(params[:category_id])
-    @article = @category.articles.new
   end
 
   def create
-    @category = Category.find(params[:category_id])
-    @article = @category.articles.new(article_params)
+    @article.attributes = article_params.merge({user: current_user})
     if @article.save
       redirect_to category_article_path(@category, @article)
     else
@@ -18,36 +18,43 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:category_id])
-    @article = @category.articles.find(params[:id])
   end
 
   def edit
-    @category = Category.find(params[:category_id])
-    @article = @category.articles.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:category_id])
-    @article = @category.articles.find(params[:id])
-    if @article.update(article_params)
-      redirect_to category_article_path(@category, @article)
+    if @article && @article.update(article_params)
+      redirect_to category_article_path(@article.category_id, @article)
     else
       render 'edit'
     end
   end
 
   def destroy
-    @category = Category.find(params[:category_id])
-    @article = @category.articles.find(params[:id])
     @article.destroy
     redirect_to @category
   end
 
   private
 
+  def set_article
+    @article = if params[:id]
+      @category.articles.find_by(id: params[:id])
+    else
+      @category.articles.new
+    end
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_user_article
+    @article = current_user.articles.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
-
 end
