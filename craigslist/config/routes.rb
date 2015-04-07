bfa2379c05bda332001854539e218f809da96a8c
Jose Roman <jferoman@gmail.com>
2015-04-06 21:51:13 -0400

    clarify shit

--- craigslist/config/routes.rb
+++ craigslist/config/routes.rb
@@ -4,9 +4,27 @@ Rails.application.routes.draw do
     resources :articles, except: [:index]
   end
 
-  resources :users, only: [:new, :create]
+ 
 
-  resource :session, only: [:new, :create, :destroy]
+  # resources :users, only: [:new, :create]
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
+
+
+# reources way
+# resources :users, only: [:new, :create]
+# users POST /users(.:format)     users#create
+# new_user GET  /users/new(.:format) users#new
+# long way
+# get '/users/new' => 'users#new'
+# post '/users' => 'users#create'
+# users_new GET  /users/new(.:format) users#new
+# users POST /users(.:format)     users#create
+
+  # resource :session, only: [:new, :create, :destroy]
+  get  'session/new' => 'sessions#new'
+  post 'session' => 'sessions#create'
+  delete 'session' => 'sessions#destroy'
 
   root 'categories#index'
 
